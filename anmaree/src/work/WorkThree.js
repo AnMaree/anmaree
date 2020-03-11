f5cb343c63ef84a73c6b46ec9a6675479274bd8b
AnMaree Williams <anmaree@seedinvest.com>
2020-03-10 20:09:12 -0400

    resume update and wwork 3

--- anmaree/src/work/WorkThree.js
+++ anmaree/src/work/WorkThree.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import Hero from '../imgs/work-three/hero.png';
-import Launch from '../imgs/work-three/launch.jpg';
+import Launch from '../imgs/work-three/launch.png';
+
+import Wireframes from '../imgs/work-three/wireframes.jpg';
+import Prototypes from '../imgs/work-three/mockups.png';
+
+import PersonaOrganizer from '../imgs/work-three/persona-organizer.png';
+import PersonaFounder from '../imgs/work-three/persona-founder.png';
+import PersonaInvestor from '../imgs/work-three/persona-investor.png';
 
 import FeedbackOne from '../imgs/work-three/feedback-01.png';
 import FeedbackTwo from '../imgs/work-three/feedback-02.png';
@@ -13,8 +20,6 @@ import FeedbackSeven from '../imgs/work-three/feedback-07.png';
 import FeedbackEight from '../imgs/work-three/feedback-08.png';
 
 import workCoverOneSmall from '../imgs/work-one-cover-small.jpg';
-import workCoverTwoSmall from '../imgs/work-two-cover-small.jpg';
-import workCoverThreeSmall from '../imgs/work-three-cover-small.jpg';
 import workCoverFourSmall from '../imgs/work-four-cover-small.jpg';
 import workCoverFiveSmall from '../imgs/work-five-cover-small.jpg';
 
@@ -64,7 +69,7 @@ class WorkOne extends Component {
                           <h5 className="snippet-label">Project Details</h5>
                         </div>
                         <div className="snippet-content">
-                          <h3>Lead Product Designer</h3>
+                          <h3 className="text-left">Lead Product Designer</h3>
                           <p>Product Planning, User Research, Stakeholder Interviews, Interaction design, Visual design, Prototyping, Testing, Front-End Development.</p>
                           <hr/>
                           <p>1 Designer, 4 Developers, 1 Project Manager</p>
@@ -85,21 +90,19 @@ class WorkOne extends Component {
               <div className="row">
                 <div className="col-12">
                   <section className="mb-5">
-                    <div className="section-header mb-5 col-md-10 mx-auto text-center">
+                    <div className="section-header text-center mb-5 col-md-10 mx-auto">
                       <h1 className="display-4 mb-4">A Reflection On Our First Event</h1>
                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
-                    <div className="section-content">
-                      <img className="launch-img col-md-9 mx-auto d-block" src={Launch} alt="launch" />
+                    <div className="section-content mt-7 col-lg-10 mx-auto text-center">
+                      <img className="launch-img w-100" src={Launch} alt="launch" />
                     </div>             
                   </section>
                   <section>
                     <div className="section-content mt-7 col-lg-10 mx-auto text-center">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                      <hr className="my-5" />
                       <div className="row mt-5">
                         <div className="col">
-                          <div className="snippet">
+                          <div className="snippet bg-white box-shadow py-5">
                             <div className="snippet-header">
                               <div className="snippet-title display-4 font-weight-bold">During</div>
                               <p>The Event</p>
@@ -107,7 +110,7 @@ class WorkOne extends Component {
                           </div>
                         </div>
                         <div className="col">
-                          <div className="snippet">
+                          <div className="snippet bg-white box-shadow py-5">
                             <div className="snippet-header">
                               <div className="snippet-title display-4 font-weight-bold">148</div>
                               <p>Investments</p>
@@ -115,7 +118,7 @@ class WorkOne extends Component {
                           </div>
                         </div>
                         <div className="col">
-                          <div className="snippet">
+                          <div className="snippet bg-white box-shadow py-5">
                             <div className="snippet-header">
                               <div className="snippet-title display-4 font-weight-bold">$223,188</div>
                               <p>Amount Raised</p>
@@ -130,7 +133,7 @@ class WorkOne extends Component {
                     <div className="section-content mt-5 col-lg-10 mx-auto text-center">
                       <div className="row">
                         <div className="col">
-                          <div className="snippet">
+                          <div className="snippet bg-white box-shadow py-5">
                             <div className="snippet-header">
                               <div className="snippet-title display-4 font-weight-bold">After</div>
                               <p>The Event</p>
@@ -138,7 +141,7 @@ class WorkOne extends Component {
                           </div>
                         </div>
                         <div className="col">
-                          <div className="snippet">
+                          <div className="snippet bg-white box-shadow py-5">
                             <div className="snippet-header">
                               <div className="snippet-title display-4 font-weight-bold">2,019</div>
                               <p>Investments</p>
@@ -146,7 +149,7 @@ class WorkOne extends Component {
                           </div>
                         </div>
                         <div className="col">
-                          <div className="snippet">
+                          <div className="snippet bg-white box-shadow py-5">
                             <div className="snippet-header">
                               <div className="snippet-title display-4 font-weight-bold">$76,200.00</div>
                               <p>Average Daily Raised</p>
@@ -158,8 +161,6 @@ class WorkOne extends Component {
                   </section>
                   <section>
                     <div className="section-content mt-7 col-lg-10 mx-auto text-center">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                      <hr className="my-5" />
                       <div className="row mt-5">
                         <div className="col-6 col-md-4 col-lg-3">
                           <div className="feedback-img-wrapper mb-5 box-shadow">
@@ -213,43 +214,54 @@ class WorkOne extends Component {
           <div className="container">
             <div className="section-content">
               <div className="row">
-                <div className="col-12">
-                  <section className="mb-5">
-                    <div className="section-header mb-5 col-md-10 mx-auto">
-                      <h1 className="display-4 mb-4">The Problem</h1>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div className="col-10 col-md-12 mx-auto">
+                  <section>
+                    <div className="section-header mb-5 text-center">
+                      <h1 className="display-4 mb-4">The Problems</h1>
                     </div>
-                  </section>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="py-10 bg-bb-blue">
-          <div className="container">
-            <div className="section-content">
-              <div className="row">
-                <div className="col-12">
-                  <section className="mb-5">
-                    <div className="section-header mb-5 col-md-10 mx-auto">
-                      <h1 className="display-4 mb-4">The Solution</h1>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                  </section>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="py-10">
-          <div className="container">
-            <div className="section-content">
-              <div className="row">
-                <div className="col-12">
-                  <section className="mb-5">
-                    <div className="section-header mb-5 col-md-10 mx-auto text-center">
-                      <h1 className="display-4 mb-4">Types of Events</h1>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <div className="section-content mt-5 text-center">
+                      <div className="row">
+                        <div className="col-12 col-md-6 col-lg-3">
+                          <section className="my-5 p-5 border">
+                            <div className="section-header">
+                              <h4 className="section-title display-4 font-weight-bold">#1</h4>
+                            </div>
+                            <div className="section-content">
+                              <p>Activity during the event was insignificant compared to after the event.</p>
+                            </div>
+                          </section>
+                        </div>
+                        <div className="col-12 col-md-6 col-lg-3">
+                          <section className="my-5 p-5 border">
+                            <div className="section-header">
+                              <h4 className="section-title display-4 font-weight-bold">#2</h4>
+                            </div>
+                            <div className="section-content">
+                              <p>Investors experience confirmation bias and loss aversion.</p>
+                            </div>
+                          </section>
+                        </div>
+                        <div className="col-12 col-md-6 col-lg-3">
+                          <section className="my-5 p-5 border">
+                            <div className="section-header">
+                              <h4 className="section-title display-4 font-weight-bold">#3</h4>
+                            </div>
+                            <div className="section-content">
+                              <p>Based on founder and organizer feedback, our core mission was lost.</p>
+                            </div>
+                          </section>
+                        </div>
+                        <div className="col-12 col-md-6 col-lg-3">
+                          <section className="my-5 p-5 border">
+                            <div className="section-header">
+                              <h4 className="section-title display-4 font-weight-bold">#4</h4>
+                            </div>
+                            <div className="section-content">
+                              <p>The problems to solve are with the issuer, not he organization.</p>
+                            </div>
+                          </section>
+                        </div>
+                      </div>
                     </div>
                   </section>
                 </div>
@@ -263,12 +275,114 @@ class WorkOne extends Component {
               <div className="row">
                 <div className="col-12">
                   <section className="mb-5">
-                    <div className="section-header mb-5 col-md-10 mx-auto text-center">
-                      <h1 className="display-4 mb-4">Process Work</h1>
+                    <div className="section-header text-center mb-5 col-md-10 mx-auto">
+                      <h1 className="display-4 mb-4">Empathize & Define Our Users</h1>
                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div className="section-content mt-7 my-5 col-xl-10 mx-auto text-center">
+                      <img className="persona-img w-100 box-shadow" src={PersonaOrganizer} alt="launch" />
+                    </div>
+                    <div className="section-content my-5 col-xl-10 mx-auto text-center">
+                      <img className="persona-img w-100 box-shadow" src={PersonaFounder} alt="launch" />
+                    </div>
+                    <div className="section-content my-5 col-xl-10 mx-auto text-center">
+                      <img className="persona-img w-100 box-shadow" src={PersonaInvestor} alt="launch" />
+                    </div>          
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="py-10">
+          <div className="container">
+            <div className="section-content">
+              <div className="row">
+                <div className="col-10 mx-auto">
+                  <section>
+                    <div className="section-header mb-5 text-center">
+                      <h1 className="display-4 mb-4">The Solutions</h1>
+                    </div>
+                    <div className="section-content mt-5 text-center">
+                      <div className="row">
+                        <div className="col-12 col-md-6">
+                          <section className="my-5 p-5 border border-dark">
+                            <div className="section-header">
+                              <p className="section-title font-weight-bold">Redefine SeedInvest "Live" Concept.</p>
+                            </div>
+                            <div className="section-content">
+                              <p>It should support our brand’s mission to transform the broken private company fundraising process in order to make it easier for entrepreneurs to raise capital. Value for the organization will follow.</p>
+                            </div>
+                          </section>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <section className="my-5 p-5 border border-dark">
+                            <div className="section-header">
+                              <p className="section-title font-weight-bold">Create a mobile friendly experience.</p>
+                            </div>
+                            <div className="section-content">
+                              <p>Investors need more information, education, and preparation before events start to know what “Live” investing is, why they should get involved, and be given the tools to make thoughtful decisions.</p>
+                            </div>
+                          </section>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <section className="my-5 p-5 border border-dark">
+                            <div className="section-header">
+                              <p className="section-title font-weight-bold">Make the investor’s experience as frictionless as possible upfront and convert later.</p>
+                            </div>
+                            <div className="section-content">
+                              <p>Give more options and features to “Subscribe” and /or “Ask a Question”, rather than push to “Invest Now”.</p>
+                            </div>
+                          </section>
+                        </div>
+                        <div className="col-12 col-md-6">
+                          <section className="my-5 p-5 border border-dark">
+                            <div className="section-header">
+                              <p className="section-title font-weight-bold">Enable the venture team to create event pages with no intervention from Product.</p>
+                            </div>
+                            <div className="section-content">
+                              <p>Founders should know who is interested in their company at an event, know who is accredited.</p>
+                            </div>
+                          </section>
+                        </div>
+                      </div>
                     </div>
                   </section>
                 </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="pt-8 bg-dark">
+          <div className="section-content">
+            <div className="row">
+              <div className="col-12">
+                <section>
+                  <div className="section-header text-center text-white col-md-6 mx-auto">
+                    <h1 className="display-4 mb-4">Ideating & Wireframing</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  </div>
+                  <div className="section-content text-center">
+                    <img className="wireframe-img w-100" src={Wireframes} alt="wireframe" />
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="pt-8 bg-light">
+          <div className="section-content">
+            <div className="row">
+              <div className="col-12">
+                <section>
+                  <div className="section-header text-center col-md-6 mx-auto">
+                    <h1 className="display-4 mb-4">Mockups & Prototypes</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  </div>
+                  <div className="section-content text-center">
+                    <img className="prototypes-img w-100" src={Prototypes} alt="prototypes" />
+                  </div>
+                </section>
               </div>
             </div>
           </div>
